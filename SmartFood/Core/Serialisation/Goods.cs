dc99656b1dbac51afbe6de93df698f508e41edf0
Irdis <Irdis@IRDIS-PC>
2016-12-17 16:08:19 +0200

    good serialization

---
+++ SmartFood/Core/Serialisation/Goods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartFood.Core.Serialisation
+{
+    [Serializable]
+    public class Goods
+    {
+        public int count;
+        public List<Good> items;
+    }
+
+    [Serializable]
+    public class Good
+    {
+        public int id;
+        public int id_category;
+        public string name;
+        public double weight;
+        public double price;
+        public string category;
+        public int visible;
+        [NonSerialized]
+        public GoodConsumbles consumbles;
+    }
+}
